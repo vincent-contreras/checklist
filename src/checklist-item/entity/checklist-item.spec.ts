43fbbefa8e21c091275af7af4e85d91229799ab0
Paul Vincent Contreras <vincent.contreras@outlook.com>
2021-04-15 17:26:24 +0900

    Add entity spec

---
+++ src/checklist-item/entity/checklist-item.spec.ts
@@ -0,0 +1,9 @@
+import { ChecklistItem } from './checklist-item.entity';
+
+describe('ChecklistItemEntity', () => {
+  it('Item NOT null', () => {
+    const checklistItem = new ChecklistItem();
+    expect(checklistItem).toBeTruthy();
+    expect(checklistItem.item).toBeUndefined()
+  });
+});
--- src/checklist-item/entity/checklist-item.specter.ts
+++
@@ -1,7 +0,0 @@
-// import { ChecklistItemEntity } from './checklist-item.entity';
-//
-// describe('ChecklistItemEntity', () => {
-//   // it('should be defined', () => {
-//   //   // expect(new ChecklistItemEntity()).toBeDefined();
-//   // });
-// });
