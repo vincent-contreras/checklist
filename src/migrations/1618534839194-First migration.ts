bfdf39170c40b094b57900f5220f15bb16a2c34c
Paul Vincent Contreras <vincent.contreras@outlook.com>
2021-04-16 11:22:53 +0900

    Linting

--- src/migrations/1618534839194-First migration.ts
+++ src/migrations/1618534839194-First migration.ts
@@ -1,14 +1,15 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class FirstMigration1618534839194 implements MigrationInterface {
-    name = 'FirstMigration1618534839194'
+  name = 'FirstMigration1618534839194';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("CREATE TABLE `checklist_item` (`id` int NOT NULL AUTO_INCREMENT, `item` varchar(255) NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB");
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      'CREATE TABLE `checklist_item` (`id` int NOT NULL AUTO_INCREMENT, `item` varchar(255) NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB',
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("DROP TABLE `checklist_item`");
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('DROP TABLE `checklist_item`');
+  }
 }
